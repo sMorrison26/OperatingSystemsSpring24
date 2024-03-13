4bf6524a1be086fe232da31c8cfa875d66a6e7fa
Hitgan0 <ehitgano2002@gmail.com>
2024-03-13 11:53:57 -0400

    Read in args for proj pt 1

--- main.c
+++ main.c
@@ -3,8 +3,14 @@
 #include <stddef.h>
 
 int main(int argc, char** argv){
+  int n = atoi(argv[1]);
+  int NCPU = atoi(argv[2]);
+  int seed = atoi(argv[3]);
+  float lambda = atof(argv[4]);
+  int upperBound =atoi(argv[5]);
   
-  
+  // Check to see if command line args are parsed properly
+  // printf("%d, %d, %d, %.3f, %d\n", n, NCPU, seed, lambda, upperBound);
   
   return 0;
 }
